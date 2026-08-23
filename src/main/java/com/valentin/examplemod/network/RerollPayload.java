2ba1b560f2846dc0598cc1daf4165e6c2f12649e
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-23 15:12:31 -0300

    Refactor RerollPayload codec implementation

--- src/main/java/com/valentin/examplemod/network/RerollPayload.java
+++ src/main/java/com/valentin/examplemod/network/RerollPayload.java
@@ -3,12 +3,15 @@ package com.valentin.examplemod.network;
 import com.valentin.examplemod.ExampleMod;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
 public record RerollPayload(int villagerId) implements CustomPayload {
     public static final Id<RerollPayload> ID = new Id<>(Identifier.of(ExampleMod.MOD_ID, "reroll"));
     public static final PacketCodec<PacketByteBuf, RerollPayload> CODEC =
-        PacketCodec.tuple(PacketCodec.INTEGER, RerollPayload::villagerId, RerollPayload::new);
-    @Override public Id<? extends CustomPayload> getId() { return ID; }
+        PacketCodecs.INTEGER.xmap(RerollPayload::new, RerollPayload::villagerId).cast();
+
+    @Override
+    public Id<? extends CustomPayload> getId() { return ID; }
 }
