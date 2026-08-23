6d9b1d55089ef24258f3b6fe4b3a050bb2fa971b
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-23 17:17:17 -0300

    Change MerchantEntity to Merchant in accessor method

--- src/main/java/com/valentin/examplemod/mixin/MerchantScreenHandlerAccessor.java
+++ src/main/java/com/valentin/examplemod/mixin/MerchantScreenHandlerAccessor.java
@@ -1,12 +1,12 @@
 package com.valentin.examplemod.mixin;
 
-import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.screen.MerchantScreenHandler;
+import net.minecraft.village.Merchant;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MerchantScreenHandler.class)
 public interface MerchantScreenHandlerAccessor {
     @Accessor("merchant")
-    MerchantEntity getMerchant();
+    Merchant getMerchant();
 }
