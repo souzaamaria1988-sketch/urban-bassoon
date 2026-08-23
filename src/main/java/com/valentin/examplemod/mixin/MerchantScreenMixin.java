66b80eb7da64ec0a92b65d305ef681e15c46af72
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-23 16:06:41 -0300

    Update MerchantScreenMixin.java

--- src/main/java/com/valentin/examplemod/mixin/MerchantScreenMixin.java
+++ src/main/java/com/valentin/examplemod/mixin/MerchantScreenMixin.java
@@ -1,17 +1,19 @@
 package com.valentin.examplemod.mixin;
 
+import com.valentin.examplemod.ExampleMod;
 import com.valentin.examplemod.network.RerollPayload;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = MerchantScreen.class, priority = 1500) // Prioridade máxima
+@Mixin(value = MerchantScreen.class, priority = 2000)
 public abstract class MerchantScreenMixin extends Screen {
 
     protected MerchantScreenMixin(Text title) { super(title); }
@@ -20,9 +22,22 @@ public abstract class MerchantScreenMixin extends Screen {
     private void addRerollButton(CallbackInfo ci) {
         MerchantScreen self = (MerchantScreen)(Object)this;
 
-        ButtonWidget btn = ButtonWidget.builder(Text.literal("§a⟳ Reroll"), b -> {
-            int id = self.getScreenHandler().syncId;
-            ClientPlayNetworking.send(new RerollPayload(id));
+        ButtonWidget btn = ButtonWidget.builder(Text.literal("§a⟳ Reroll"), button -> {
+            try {
+                MerchantScreenHandlerAccessor handler = 
+                    (MerchantScreenHandlerAccessor) self.getScreenHandler();
+                MerchantEntity merchant = handler.getMerchant();
+                
+                if (merchant != null) {
+                    int entityId = merchant.getId();
+                    ExampleMod.LOGGER.info("Sending reroll for entity ID: {}", entityId);
+                    ClientPlayNetworking.send(new RerollPayload(entityId));
+                } else {
+                    ExampleMod.LOGGER.error("Merchant is null!");
+                }
+            } catch (Exception e) {
+                ExampleMod.LOGGER.error("Error sending reroll packet", e);
+            }
         }).dimensions(self.width / 2 + 95, self.height / 2 - 80, 60, 20).build();
 
         this.addDrawableChild(btn);
