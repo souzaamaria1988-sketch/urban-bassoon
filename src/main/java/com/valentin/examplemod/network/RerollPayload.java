dad3cf9c1ec497e63ca18c08e22269d6c6388244
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-23 14:59:00 -0300

    Add RerollPayload class for network communication

---
+++ src/main/java/com/valentin/examplemod/network/RerollPayload.java
@@ -0,0 +1,23 @@
+package com.valentin.examplemod.network;
+
+import com.valentin.examplemod.ExampleMod;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.util.Identifier;
+
+public record RerollPayload(int villagerId) implements CustomPayload {
+    public static final CustomPayload.Id<RerollPayload> ID = 
+        new CustomPayload.Id<>(Identifier.of(ExampleMod.MOD_ID, "reroll"));
+    
+    public static final PacketCodec<PacketByteBuf, RerollPayload> CODEC = 
+        PacketCodec.tuple(
+            PacketCodec.INTEGER, RerollPayload::villagerId,
+            RerollPayload::new
+        );
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+}
