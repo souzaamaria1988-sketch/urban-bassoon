1fc9e58b84c46242d9742034a7d66a4aa8bd6c4e
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-23 15:41:14 -0300

    Add accessors for VillagerEntity fields

--- src/main/java/com/valentin/examplemod/mixin/VillagerEntityAccessor.java
+++ src/main/java/com/valentin/examplemod/mixin/VillagerEntityAccessor.java
@@ -3,10 +3,20 @@ package com.valentin.examplemod.mixin;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(VillagerEntity.class)
 public interface VillagerEntityAccessor {
     @Invoker("fillRecipes")
     void invokeFillRecipes(ServerWorld world);
+    
+    @Accessor("lastRestockTime")
+    void setLastRestockTime(long time);
+    
+    @Accessor("restocksToday")
+    void setRestocksToday(int count);
+    
+    @Accessor("lastRestockCheckTime")
+    void setLastRestockCheckTime(long time);
 }
