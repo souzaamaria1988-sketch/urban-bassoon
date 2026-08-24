bf7a62bd2c3c7cc343f870328b12d00652f731dd
souzaamaria1988-sketch <souzaamaria1988@gmail.com>
2026-08-24 12:05:24 -0300

    Remove villager reroll button functionality
    Removed the reroll button logic and associated error handling.

--- src/main/java/com/valentin/examplemod/handler/VillagerRerollHandler.java
+++ src/main/java/com/valentin/examplemod/handler/VillagerRerollHandler.java
@@ -45,28 +45,4 @@ public class VillagerRerollHandler {
         
         ExampleMod.LOGGER.info("Reroll completed for villager {}", villager.getId());
     }
-}            try {
-                int entityId = ExampleMod.lastInteractedMerchantId;
-                
-                if (entityId != -1) {
-                    ExampleMod.LOGGER.info("Sending reroll for villager ID: {}", entityId);
-                    ClientPlayNetworking.send(new RerollPayload(entityId));
-                } else {
-                    if (client.player != null) {
-                        client.player.sendMessage(
-                            Text.literal("§c[ExampleMod] §fFeche e abra o inventário do aldeão novamente!"), 
-                            false
-                        );
-                    }
-                }
-            } catch (Exception e) {
-                ExampleMod.LOGGER.error("Error sending reroll packet", e);
-            }
-        })
-        .dimensions(buttonX, buttonY, 60, 20)
-        .build();
-
-        this.addDrawableChild(btn);
-        ExampleMod.LOGGER.info("Reroll button added at ({}, {})", buttonX, buttonY);
-    }
-} 
+}
